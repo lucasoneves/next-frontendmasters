fd6484e123e71982b1c7c29d4297ffd2236f7828
Lucas Neves <lucasdeoliveiraneves@Mac-mini-de-Lucas.local>
2023-03-02 17:58:29 -0300

    Isolating the button component as a client component

---
+++ app/components/Button/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+"use client"
+
+import { useRouter } from 'next/navigation';
+
+export default function Button() {
+  const router = useRouter()
+  return (
+    <button type="button" onClick={() => router.push("/blog")}>
+      Blog
+    </button>
+  );
+}
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,15 +1,11 @@
-"use client";
 import React from "react";
-import { useRouter } from "next/navigation";
+import Button from "app/components/Button/page";
 
 export default function Contact() {
-  const router = useRouter();
   return (
     <div>
       <h2>Contact me</h2>
-      <button type="button" onClick={() => router.push("/blog")}>
-        Blog
-      </button>
+      <Button />
     </div>
   );
 }
